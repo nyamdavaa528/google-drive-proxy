1b57782076e203c461692453cea6e4e0744a0154
nyamdavaa528 <fiance0408@gmail.com>
2025-05-19 22:56:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const request = require('request');
+const https = require('https');
+const { pipeline } = require('stream');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,16 +11,18 @@ app.get('/stream/:id', (req, res) => {
   const fileId = req.params.id;
   const driveUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
 
-  request
-    .get(driveUrl)
-    .on('response', (r) => {
-      // Force browser to treat it as inline stream
-      res.setHeader('Content-Type', r.headers['content-type']);
-      res.setHeader('Content-Disposition', 'inline'); // ✅ энэ мөр нэмэгдэнэ
-    })
-    .pipe(res);
-});
+  https.get(driveUrl, (driveRes) => {
+    res.setHeader('Content-Type', driveRes.headers['content-type'] || 'video/mp4');
+    res.setHeader('Content-Disposition', 'inline');
 
+    pipeline(driveRes, res, (err) => {
+      if (err) {
+        console.error('Pipeline error:', err);
+        res.sendStatus(500);
+      }
+    });
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
