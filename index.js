f5b33b8bf8affb450efa8cff165cb90f116d31ac
nyamdavaa528 <fiance0408@gmail.com>
2025-05-19 22:40:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-index.js
+const express = require('express');
+const request = require('request');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Google Drive proxy is running');
+});
+
+app.get('/stream/:id', (req, res) => {
+  const fileId = req.params.id;
+  const driveUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+  request
+    .get(driveUrl)
+    .on('response', (r) => {
+      res.setHeader('Content-Type', r.headers['content-type']);
+    })
+    .pipe(res);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
