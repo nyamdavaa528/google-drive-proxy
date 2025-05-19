24f1525ea7684f13a84906e46bb3ab6675bcda1f
nyamdavaa528 <fiance0408@gmail.com>
2025-05-19 22:52:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ app.get('/stream/:id', (req, res) => {
   request
     .get(driveUrl)
     .on('response', (r) => {
+      // Force browser to treat it as inline stream
       res.setHeader('Content-Type', r.headers['content-type']);
+      res.setHeader('Content-Disposition', 'inline'); // ✅ энэ мөр нэмэгдэнэ
     })
     .pipe(res);
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
